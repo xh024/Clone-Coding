ef801477f736d0d24fc2a75dac1c5c77f7cf7073
xh024 <jeongho4781@gmail.com>
2023-04-22 22:23:47 +0900

    #2.8 Functions part Two

--- Vanilla_js-chrome/app.js
+++ Vanilla_js-chrome/app.js
@@ -1,8 +1,37 @@
 // function
-function sayHello(){
-  console.log("Hello my name is ");
+// function에 argument 여러개 설정 가능
+function sayHello(nameOfPerson, age) {
+  console.log("Hello my name is " + nameOfPerson + " and I'm " + age);
 }
 
-sayHello("nico");
-sayHello("dal");
-sayHello("lynn");
+sayHello("nico", 10);
+sayHello("dal", 23);
+sayHello("lynn", 21);
+
+
+
+// 계산기 만들기
+function plus(firstNunmber, secondNumber) {
+  console.log(firstNunmber + secondNumber);
+}
+function divide(firstNumber, secondNumber) {
+  console.log(firstNumber / secondNumber);
+}
+
+plus(80, 2);
+divide(98, 20);
+
+
+
+// object와 function 결합
+const player = {
+  name: "jeongho",
+  sayHello: function(otherPersonName) {
+    console.log("hello! " + otherPersonName + " nice to meet you");
+  }
+}
+
+console.log(player.name);
+player.sayHello("lynn");
+player.sayHello("nico");
+player.sayHello("dal");
