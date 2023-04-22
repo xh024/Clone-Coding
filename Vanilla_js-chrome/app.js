c65cb9cb36b2f1f4c88436a4e4629a767f0e6433
xh024 <jeongho4781@gmail.com>
2023-04-22 12:44:57 +0900

    #2.4 Booleans

--- Vanilla_js-chrome/app.js
+++ Vanilla_js-chrome/app.js
@@ -1,15 +1,6 @@
-// variable
-// const and let
-// 기본적으로 변수를 설정할 때 const를 사용하고, let은 필요할 때만, var는 되도록 사용하지 말 것.
-const a = 5;
-const b = 2;
-let myName = "jeongho"
+// boolean
+// true(켜져있음), flase(꺼져있음), null(값이 없음), undefined(variable이 정의되어 있지만, 값이 정해지지 않음)
+const amIFat = null;
+let something;
 
-console.log(a + b);
-console.log(a * b);
-console.log(a / b);
-console.log("hello " + myName);
-
-myName = "Kim-JeongHo"; //let은 코드를 적으면서 업데이트 가능.
-
-console.log("your name is " + myName);
+console.log(something, amIFat);
