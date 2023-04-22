37354e0201176c048ece8b4713a947bc0ddcf654
xh024 <jeongho4781@gmail.com>
2023-04-22 17:56:11 +0900

    #2.6 Objects

--- Vanilla_js-chrome/app.js
+++ Vanilla_js-chrome/app.js
@@ -1,10 +1,32 @@
-// arrays
-const dayOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat"];
+// object
+// 데이터 정리 방법
+const player = {
+  name: "jeongho",
+  points: 10,
+  fat: false,
+}
 
-// Get Item from Array
-console.log(dayOfWeek[4]);
+console.log(player);
 
-// Add on emore day to the array
-dayOfWeek.push("sun");
 
-console.log(dayOfWeek);
+// object 값 확인
+console.log(player.name);
+console.log(player["name"]);
+
+
+// property 변경
+player.fat = true;
+console.log(player);
+
+
+// proporty 값에 더하기
+player.points = player.points + 50;
+console.log(player);
+
+
+// property 추가
+player.lastName = "photato";
+console.log(player);
+
+
+// console == object, .log == property
