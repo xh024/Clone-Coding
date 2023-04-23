f1c8e79c0bfbdc5c71dcb3d0f940fa1bd8378a96
xh024 <jeongho4781@gmail.com>
2023-04-23 23:59:57 +0900

    #2.14 Conditionals part Two

--- Vanilla_js-chrome/app.js
+++ Vanilla_js-chrome/app.js
@@ -1,6 +1,20 @@
 //conditionals
-const age = prompt("How old are you?");
+const age = parseInt(prompt("How old are you?"));
 // prompt는 입력 값을 무조건 srting으로 처리
 
 // parseInt를 사용하면 string ----> number
-console.log(age, parseInt(age));
+// console.log(isNaN(age));
+// isNaN은 boolean(true/false)로 number인지 아닌지 판다 --> flase인 경우 number
+
+// if문 형식
+// if(condition) {
+//   // condition === true
+// } else {
+//   // condition === false
+// }
+
+if(isNaN(age)) {
+  console.log("Please write a number.");
+} else {
+  console.log("Thank you for writing your age.");
+}
