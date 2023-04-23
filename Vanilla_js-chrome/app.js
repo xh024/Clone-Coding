3fc1bd85d15c69f9664f5c18c44298f5aea88759
xh024 <jeongho4781@gmail.com>
2023-04-23 19:19:27 +0900

    #2.13 Conditionals

--- Vanilla_js-chrome/app.js
+++ Vanilla_js-chrome/app.js
@@ -1,40 +1,6 @@
-// return
-// alret, console 사용하지 않으려면 return 사용
+//conditionals
+const age = prompt("How old are you?");
+// prompt는 입력 값을 무조건 srting으로 처리
 
-// 예제 1
-const calculator = {
-  plus: function(firstNumber, secondNumber) {
-    return firstNumber + secondNumber;
-  },
-  minus: function(firstNumber, secondNumber) {
-    return firstNumber - secondNumber;
-  },
-  times: function(firstNumber, secondNumber) {
-    return firstNumber * secondNumber;
-  },
-  divide: function(firstNumber, secondNumber) {
-    return firstNumber / secondNumber;
-  },
-  power: function(firstNumber, secondNumber) {
-    return firstNumber ** secondNumber;
-  }
-}
-
-const plusResult = calculator.plus(2, 3); // 5
-const minusResult = calculator.minus(plusResult, 10); // -5
-const timesResult = calculator.times(10, minusResult); // -50
-const divideResult = calculator.divide(timesResult, plusResult); // -10
-const powerResult = calculator.power(divideResult, minusResult); // -10^5
-
-
-
-// 예제 2
-const age = 96;
-function calculatorKrAge(ageOfForeigner) {
-  ageOfForeigner + 2;
-  return "hello";
-}
-
-const krAge = calculatorKrAge(age);
-
-console.log(krAge);
+// parseInt를 사용하면 string ----> number
+console.log(age, parseInt(age));
