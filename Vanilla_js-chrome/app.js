a30f32fc668f8973b1cbcfcec8278396aa43c419
xh024 <jeongho4781@gmail.com>
2023-04-24 00:23:24 +0900

    #2.15 Conditionals part Three

--- Vanilla_js-chrome/app.js
+++ Vanilla_js-chrome/app.js
@@ -1,20 +1,26 @@
 //conditionals
 const age = parseInt(prompt("How old are you?"));
-// prompt는 입력 값을 무조건 srting으로 처리
 
-// parseInt를 사용하면 string ----> number
-// console.log(isNaN(age));
-// isNaN은 boolean(true/false)로 number인지 아닌지 판다 --> flase인 경우 number
-
-// if문 형식
-// if(condition) {
-//   // condition === true
-// } else {
-//   // condition === false
-// }
-
-if(isNaN(age)) {
+if(isNaN(age) || age < 0) {
   console.log("Please write a number.");
-} else {
-  console.log("Thank you for writing your age.");
+} else if(age < 18) {
+  console.log("You are too young.");
+} else if(age >= 18 && age <= 50) {
+  console.log("You can drink.");
+} else if(age > 50 && age <= 80) {
+  console.log("You should exercise.");
+} else if(age > 80) {
+  console.log("You can do whatever you want.");
 }
+
+// else if로 조건을 연속적으로 줄 수 있다.
+// else는 선택사항
+
+/* And 기호 === &&
+Or 기호 === || */
+
+/* 숫자가 아니고 음수이면 "Please write a number.", 
+18세 미만이면 "You are too young.", 
+18세 이상 50세 이하이면 "You can drink.", 
+50세 초과이고 80세 이하이면 "You should exercise."
+80세 초과이면 "You can do whatever you want." */
